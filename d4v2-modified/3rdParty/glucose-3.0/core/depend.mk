ec23adb567ff8403e1aa4c2df7d15e85e33de7c3
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2025-06-09 09:12:45 -0400

    Version of glucose with own makefiles

---
+++ d4v2-modified/3rdParty/glucose-3.0/core/depend.mk
@@ -0,0 +1,35 @@
+/Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Main.o /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Main.or /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Main.od /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Main.op: /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Main.cc \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../utils/System.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/IntTypes.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../utils/ParseUtils.h \
+  /usr/local/include/stdio.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../utils/Options.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Vec.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/XAlloc.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/Dimacs.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/SolverTypes.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Alg.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Alloc.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Map.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/Solver.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/BoundedQueue.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/Constants.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Heap.h
+/Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Solver.o /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Solver.or /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Solver.od /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Solver.op: \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/Solver.cc \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/Constants.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/Solver.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/BoundedQueue.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Vec.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/IntTypes.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/XAlloc.h \
+  /usr/local/include/stdio.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../core/SolverTypes.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Alg.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Alloc.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Map.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Heap.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../utils/Options.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../utils/ParseUtils.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../mtl/Sort.h \
+  /Users/bryant/repos/d4pp/d4v2/3rdParty/glucose-3.0/core/../utils/System.h
